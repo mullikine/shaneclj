066d87cd0e8815d0cb1486cea53d007721624edd
Shane Mulligan <mullikine@gmail.com>
2021-05-01 14:59:42 +1200

    01.05.21

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to shaneclj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/shaneclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns shaneclj.core-test
+  (:require [clojure.test :refer :all]
+            [shaneclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
