9c0bf95047df39961bc09043be4a7547973395c7
Shane Mulligan <mullikine@gmail.com>
2022-01-20 09:57:22 +1300

    1642625842

--- src/shaneclj/core.clj
+++ src/shaneclj/core.clj
@@ -1,5 +1,15 @@
 (ns shaneclj.core)
 
+(defn cmd
+  ""
+  [& args]
+  ;; map a vector with a shell command
+  (clojure.string/join
+   " "
+   (map (fn [s] (->
+                 (sh "q" :in s)
+                 :out)) args)))
+
 (defn foo
   "I don't do a whole lot."
   [x]
